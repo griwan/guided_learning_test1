719e4d54bcd28d97b7a108b1fe8b71fd2ab7fa20
unknown <grkhakurel@gmail.com>
2022-02-25 09:55:14 +0545

    finished week 1

---
+++ emailScrap.py
@@ -0,0 +1,23 @@
+import re
+import json
+
+f = open("websiteData.txt","r",encoding='utf-8')
+email_list = []
+for lines in f:
+    emails = re.findall(r'[\w\.-]+@[\w\.-]+',lines)
+    if len(emails)>0:
+        email_list+=emails
+
+res = {i:email_list.count(i) for i in email_list}
+
+output = {}
+for email,occ in res.items():
+    username = email.split('@')[0]
+    if re.match(r'[\w\.-]+\.[\w\.-]+',username):
+        cat = 'human'
+    else:
+        cat = 'non-human'
+    output[email] = {"Occurance":occ,"EmailType":cat}
+
+with open('result.json', 'w') as j:
+    json.dump(output, j,indent=4)
